7c3a68cfadb64b84c28f2fd282cdfbda80f61d8c
CoderDream <coderdream@gmail.com>
2014-12-02 18:19:44 +0800

    update

--- src/main/java/com/innovaee/eorder/module/service/UserRoleService.java
+++ src/main/java/com/innovaee/eorder/module/service/UserRoleService.java
@@ -22,10 +22,20 @@ public class UserRoleService extends BaseService {
 	@Resource
 	private UserDao userDao;
 
+	/**
+	 * 查找所有的用户角色关系记录
+	 * 
+	 * @return 所有的用户角色关系记录
+	 */
 	public List<UserRole> findAllUserRoles() {
 		return (List<UserRole>) userRoleDao.findAllUserRoles();
 	}
 
+	/**
+	 * 
+	 * @param userRole
+	 * @return
+	 */
 	public UserRole findUserRoleByUserRole(String userRole) {
 		return (UserRole) userRoleDao.findUserRoleByUserRole(userRole);
 	}
@@ -38,18 +48,36 @@ public class UserRoleService extends BaseService {
 		return userRoleDao.findUserRolesByUsername(userName);
 	}
 
+	/**
+	 * @param userRole
+	 */
 	public void saveUserRole(UserRole userRole) {
 		userRoleDao.saveUserRole(userRole);
 	}
 
+	/**
+	 * @param userRole
+	 */
 	public void updateUserRole(UserRole userRole) {
 		userRoleDao.updateUserRole(userRole);
 	}
 
+	/**
+	 * 根据用户角色名称移除用户角色关系记录
+	 * 
+	 * @param userRoleName
+	 *            用户角色名称
+	 */
 	public void removeUserRole(String userRoleName) {
 		userRoleDao.removeUserRole(new UserRole(userRoleName));
 	}
 
+	/**
+	 * 根据用户角色名称数组移除对应用户角色关系记录
+	 * 
+	 * @param userRoleName
+	 *            用户角色名称数组
+	 */
 	public void removeUserRoles(String[] userRoleName) {
 		int length = userRoleName.length;
 		for (int i = 0; i < length; i++) {
@@ -58,6 +86,14 @@ public class UserRoleService extends BaseService {
 
 	}
 
+	/**
+	 * 给用户的添加某个角色
+	 * 
+	 * @param role
+	 *            待移除的角色
+	 * @param user
+	 *            待修改角色的用户
+	 */
 	public UserRole saveUserRole(Role role, User user) {
 		UserRole rtnUserRole = null;
 		Role roleDB = (Role) roleDao.get(role.getRoleName());
@@ -79,6 +115,14 @@ public class UserRoleService extends BaseService {
 
 	}
 
+	/**
+	 * 将用户的某个角色移除
+	 * 
+	 * @param role
+	 *            待移除的角色
+	 * @param user
+	 *            待修改角色的用户
+	 */
 	public void removeUserRole(Role role, User user) {
 		Role roleDB = (Role) roleDao.get(role.getRoleName());
 		User userDB = (User) userDao.get(user.getUserName());
